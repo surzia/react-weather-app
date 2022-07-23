66d786c4542224fe6da6dc5e8e73fb91119ea7e7
surzia <songjie19970916@163.com>
2022-07-23 22:52:31 +0800

    🔧 build(config):add geo api url and options

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "234b67fac5msh0d10dda53af9d05p1edb13jsnc3d99526bd19",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
